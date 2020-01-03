cd2df4eb53db6c9b0061874f380c1a7d65b682bd
piqueen <ferreiraep_95@hotmail.com>
2020-01-02 23:45:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i=0; i<names.length; i++){
       var firstLetter = names[i].charAt(0);
-      if (firstLetter === "J" or names[i].toLowerCase()){
+      if (firstLetter === "J" || names[i].toLowerCase()){
         byeSpeaker.speak(names[i]);
       }
       else{
